703e9623b5caad943009b7972f87b2549c3afbb9
yena choi <geniusyena332@gmail.com>
2022-10-31 21:16:26 +0900

    delete POST & GET from server side

--- app.py
+++ app.py
@@ -13,22 +13,5 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/honeymoon", methods=["POST"])
-def search_post():
-
-    search_receive = request.form['search_give']
-
-    doc = {'search_list': search_receive}
-    db.honeymoon.insert_one(doc)
-
-    return jsonify({'msg': '검색어 리스트 저장 완료!'})
-
-
-@app.route("/honeymoon", methods=["GET"])
-def search_get():
-    search_list = list(db.honeymoon.find({}, {'_id': False}))
-    return jsonify({'search_list': 'POST 연결 완료!'})
-
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
