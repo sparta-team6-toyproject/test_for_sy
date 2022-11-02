5ce6ebd502ce396c113e4c25ae0eb42a8149dd12
yena choi <geniusyena332@gmail.com>
2022-11-02 23:15:49 +0900

    app.py 포트 추가

--- app.py
+++ app.py
@@ -49,9 +49,69 @@ def list_get():
 
 
 # 승열님
-@app.route('/sy')
-def home_sy():
-    return render_template('toy_sy.html')
+@app.route('/루체른')
+def home_lucerne():
+    return render_template('루체른.html')
+
+
+@app.route('/마드리드')
+def home_madrid():
+    return render_template('마드리드.html')
+
+
+@app.route('/로마')
+def home_rome():
+    return render_template('로마.html')
+
+
+@app.route('/아테네')
+def home_athens():
+    return render_template('아테네.html')
+
+
+@app.route('/파리')
+def home_paris():
+    return render_template('파리.html')
+
+
+@app.route('/하와이')
+def home_hawaii():
+    return render_template('하와이.html')
+
+
+@app.route('/칸쿤')
+def home_cancun():
+    return render_template('칸쿤.html')
+
+
+@app.route('/뉴욕')
+def home_newyork():
+    return render_template('뉴욕.html')
+
+
+@app.route('/몰디브')
+def home_maldives():
+    return render_template('몰디브.html')
+
+
+@app.route('/발리')
+def home_bali():
+    return render_template('발리.html')
+
+
+@app.route('/보라카이')
+def home_boracay():
+    return render_template('보라카이.html')
+
+
+@app.route('/세부')
+def home_cebu():
+    return render_template('세부.html')
+
+
+@app.route('/다낭')
+def home_danang():
+    return render_template('다낭.html')
 
 
 @app.route("/travel", methods=["GET"])
